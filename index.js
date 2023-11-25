9a44844a39536a4213853c7bcdab34466f601c51
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-26 00:27:46 +0100

    NetworkView

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
-var w = c.width = window.innerWidth,
-    h = c.height=window.innerHeight,
-    ctx = c.getContext("2D"),
-    opts ={
-    },
+// javascript
+d3.select();
+d3.selectAll();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    squareRange = opts.range * opts.range,
-    squareAllowed = opts.allowedDist * opts.allowedDist,
-    mostDistant = opts.depth + opts.range,
-    sinX = sinY = 0,
-    cosX = cosY = 0,
-
-    
+d3.select("h1").style("color", "red");
