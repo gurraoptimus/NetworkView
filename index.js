ffabd94fb0f4e8dc43cdfdbf4e327ce0b499da06
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-26 00:46:22 +0100

    NetworkView

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ d3.select("body").append("p").text("First Paragraph");
 d3.select("body").append("p").text("Second Paragraph");
 d3.select("body").append("p").text("Third Paragraph");
 
-d3.selectAll("p");
+d3.selectAll("p").style("color", "blue");
