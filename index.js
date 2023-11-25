3d1020876ab6754818b5818fd3686a4181550dd2
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-26 00:43:02 +0100

    NetworkView

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ d3.select("h1").style("color", "red")
 .text("Updated h1 tag");
 
 d3.select("body").append("p").text("First Paragraph");
+d3.select("body").append("p").text("Second Paragraph");
+d3.select("body").append("p").text("Third Paragraph");
+
+d3.selectAll("p");
